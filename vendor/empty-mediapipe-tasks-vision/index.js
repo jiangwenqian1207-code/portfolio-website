232c4ab7f993b014a9d38fa8cce9cedb748cc60e
jiangwenqian1207-code <jiangwenqian1207@gmail.com>
2026-07-31 07:46:33 +0800

    Update compressed portfolio files

---
+++ vendor/empty-mediapipe-tasks-vision/index.js
@@ -0,0 +1,11 @@
+export class FilesetResolver {
+  static forVisionTasks() {
+    throw new Error("@mediapipe/tasks-vision is not bundled in this portfolio build.");
+  }
+}
+
+export class FaceLandmarker {
+  static createFromOptions() {
+    throw new Error("@mediapipe/tasks-vision is not bundled in this portfolio build.");
+  }
+}
